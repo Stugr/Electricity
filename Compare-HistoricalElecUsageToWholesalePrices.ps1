b4460957e7c44957c89053fca3ea25b7a34daac5
stugr <nick@stugr.com>
2020-01-15 14:35:44 +1100

    Summarise usage data

--- Compare-HistoricalElecUsageToWholesalePrices.ps1
+++ Compare-HistoricalElecUsageToWholesalePrices.ps1
@@ -10,3 +10,25 @@ $header[1] = "IntervalDate"
 # get data from csv ignoring first 2 rows and supplying custom headers (can't use import-csv because of this)
 # we only want recordIndicator 300 which is Interval data record
 $usageData = Get-Content $usageCsv | Select-Object -Skip 2 | Out-String | ConvertFrom-Csv -Header $header | ? { $_.recordIndicator -eq 300 }
+
+# create array of objects to hold summarisation of each internalValue
+$usageSummarised = 1..48 | % { [pscustomobject][ordered]@{
+    "name" = "IntervalValue$_"
+    "count" = 0
+    "average" = 0;
+    "sum" = 0;
+    "maximum" = 0;
+    "minimum" = 0;
+}}
+
+# loop through our 48 internalvalues to summarise
+foreach ($obj in $usageSummarised) {
+    # get summary for all values in each column
+    $measured = $usageData.($obj.name) | Measure-Object -Average -Sum -Maximum -Minimum
+    $obj.count = $measured.Count
+    $obj.average = $measured.Average
+    $obj.sum = $measured.Sum
+    $obj.maximum = $measured.Maximum
+    $obj.minimum = $measured.Minimum
+}
+
