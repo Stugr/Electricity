5374b2b1efbd9d6d93655c3ece4ebc4819fd3a19
stugr <nick@stugr.com>
2020-01-15 15:07:35 +1100

    Download historical wholesale pricing

---
+++ Get-HistoricalElecWholesalePricing.ps1
@@ -0,0 +1,20 @@
+﻿# Download data from https://www.aemo.com.au/Electricity/National-Electricity-Market-NEM/Data-dashboard#aggregated-data
+$state = "VIC"
+$years = 2018,2019
+
+# create prices dir
+$dir = "$PSScriptRoot\prices"
+New-Item -Path $dir -Type Directory -Force | Out-Null
+
+# loop through years
+foreach ($year in $years) {
+    # loop through months
+    1..12 | % {
+        # pad month to 2 places
+        $month = "{0:00}" -f $_
+        $file = "PRICE_AND_DEMAND_$year$($month)_$($state)1.csv"
+        # download file
+        Invoke-WebRequest -Uri "https://www.aemo.com.au/aemo/data/nem/priceanddemand/$file" -OutFile (Join-Path $dir $file)
+    }
+}
+
