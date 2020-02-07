e57d48fb873e23303b5fbbca7a21602d331ceba5
stugr <nick@stugr.com>
2020-02-07 13:42:26 +1100

    Only try and download months that are not in the future

--- Get-HistoricalElecWholesalePrices.ps1
+++ Get-HistoricalElecWholesalePrices.ps1
@@ -1,6 +1,6 @@
 ﻿# Download data from https://www.aemo.com.au/Electricity/National-Electricity-Market-NEM/Data-dashboard#aggregated-data
 $state = "VIC"
-$years = 2018,2019
+$years = 2018,2019,2020
 
 # create prices dir
 $pricesDir = "$PSScriptRoot\prices"
@@ -12,9 +12,18 @@ foreach ($year in $years) {
     1..12 | % {
         # pad month to 2 places
         $month = "{0:00}" -f $_
-        $file = "PRICE_AND_DEMAND_$year$($month)_$($state)1.csv"
-        # download file
-        Invoke-WebRequest -Uri "https://www.aemo.com.au/aemo/data/nem/priceanddemand/$file" -OutFile (Join-Path $pricesDir $file)
+
+        # convert month and year to datetime object
+        $pullDate = [datetime]::ParseExact("$year$($month)01", "yyyyMMdd",$null)
+
+        # don't bother trying to pull months that are in the future
+        if ($pulldate -gt (Get-Date)) {
+            break
+        } else {
+            $file = "PRICE_AND_DEMAND_$year$($month)_$($state)1.csv"
+            # download file
+            Invoke-WebRequest -Uri "https://www.aemo.com.au/aemo/data/nem/priceanddemand/$file" -OutFile (Join-Path $pricesDir $file)
+        }
     }
 }
 
